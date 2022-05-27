3c1eb57c4dcd9e4bad2e27d46151056c53264b38
narsing17 <narsingp14@gmail.com>
2022-05-27 14:55:25 +0530

    Wellcome message

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,15 @@
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+
+        System.out.println("----- Welcome to Employee Wage Computation! -----");
+
+        int empPresesent = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == empPresesent){
+            System.out.println("Employee is Present");
+        }
+        else
+            System.out.println("Employee is Absent");
+    }
+
+}
